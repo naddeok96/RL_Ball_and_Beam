11268a3dabd4cc6d4ce7bf1c3f547530f3782e17
Kyle Naddeo <naddeok@KyleNaddeo-PC.localdomain>
2019-11-15 13:19:29 -0800

    prelim

---
+++ env.py
@@ -0,0 +1,80 @@
+
+'''
+This will initalize the possible states and simulate the state transitions given an action
+'''
+
+# Imports
+import numpy as np
+
+class Beam():
+
+    def __init__(self):
+
+        super(Beam, self).__init__()
+
+        # Initialize States [p0,p,t,ang]
+        self.states = []
+        for i in range(13): # Inital ball location [p0]
+            for j in range(13): # Current ball location [p]
+                for k in range(13): # Target ball location [t]
+                    for m in range(16): # Beam angle [ang]
+                        ang = (m*6) - 45
+                        self.states.append([i,j,k,ang])
+        print("State Space Size:")
+        print(np.shape(self.states))    
+
+        # Initialize Starting States
+        self.starting_states = []
+        for i in range(13): # Current ball location [p]
+            for j in range(13): # Target ball location [t]
+                self.starting_states.append([i,i,j,0])
+        print("Starting State Space Size:")
+        print(np.shape(self.starting_states))
+
+        # Initialize Terminal States
+        self.terminal_states = []
+        for i in range(13): # Target ball location [t]
+            self.terminal_states.append([i,i,i,0])
+        print("Terminal State Space Size:")
+        print(np.shape(self.terminal_states))
+
+        # Environment Dynamics
+        self.ACC_GRAV = 386.09 # [in/s^2]
+        self.TIME_INTERVAL = 0.25 # [s]
+
+    def step(self, state, action):
+
+        
+        # Take a time step
+        intial_position = state[1]
+
+        inital_velocity = (state[1]-state[0])/self.TIME_INTERVAL
+
+        angle = state[3] + action
+
+        acceleration = -self.ACC_GRAV*np.sin(np.deg2rad(angle))
+
+        velocity = round(inital_velocity + acceleration*self.TIME_INTERVAL)
+
+        position = self.bin_position(intial_position + (inital_velocity*self.TIME_INTERVAL) + (0.5*acceleration*(self.TIME_INTERVAL**2)))
+
+        next_state = [intial_position, position, state[2], angle]
+
+        # Display Summary
+        print("\nStep Summary")
+        print("----------------------------------------------------")
+        print("Inital Position | Inital Velocity | Initial Angle  |")
+        print("\t",intial_position, "\t|\t", inital_velocity, "\t  |\t", state[3], "\t   |")
+        print(" Final Position | Final Velocity  | Final Angle    |")
+        print("\t",position, "\t|\t", velocity, "\t  |\t", angle, "\t   |")
+        print("----------------------------------------------------")
+
+        return next_state
+
+    def bin_position(self, position):
+        
+        bins = np.linspace(0.5, 12.5, 13)
+        digitized = np.digitize(position, bins)
+
+        return digitized
+
