66364c0dfd4db6993d271d6327c622196b1fbfea
Kyle Naddeo <naddeok5@students.rowan.edu>
2020-10-28 14:21:01 -0700

    added some comments

--- animate_episode.py
+++ animate_episode.py
@@ -1,61 +1,80 @@
+from pylab import *
+import time
+from ipywidgets import interact, interactive
 
-'''
-This will build an animation of an episode
-'''
-# Imports
-from env import Beam
-from agent import Balancer
-from silence import shh
-import plotly.graph_objects as go
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
+# limit on beam angle in radians
+ulimit = 0.2
 
-# Run One Episode
-with shh():
-    env = Beam()
-    agent = Balancer(env)
-    history = agent.episode()
-
-# Summerize Episode
-print("Summary")
-angles = [0]
-ball_x = [history["Step 1"]["Inital State"][1]]
-ball_y = 0.15
-target_loc = [float([history["Step 1"]["Inital State"][2]][0]),0]
-print("Position:", ball_x[0], "Angle:",angles[0])
-for i in range(len(history)):
-    angle = history["Step " + str(i+1)]["Current State"][3]
-    position = history["Step " + str(i+1)]["Current State"][1]
-    print("Position:", position, "Angle:",angle)
-    angles.append(angle)
-    ball_x.append(position)
-
-
-# Build Animation
-fig, ax = plt.subplots(figsize=(5, 3))
-ax.set(xlim=(-1, 14), ylim=(-8, 8))
-beam = ax.plot([0, 12], [0,0], 'k', linewidth=2, markersize=12)[0]
-ball = ax.scatter(ball_x[0], ball_y)
-target = ax.scatter(target_loc[0], target_loc[1])
-plt.grid()
-
-def animate(i):
-    transformation_matrix = np.asarray([[ np.cos(np.deg2rad(angles[i])), -np.sin(np.deg2rad(angles[i]))],
-                                        [np.sin(np.deg2rad(angles[i])), np.cos(np.deg2rad(angles[i]))]])
+def draw_beam(fig, ax):
+    # Draw the ball and beam diagram on a given figure and axis
+    ax.axis('off')
+    ax.axis('equal')
+    ax.set(xlim = (-0.2,1.2), ylim = (-0.3,0.3))
     
-    [beam_x_rot, beam_y_rot] = transformation_matrix.dot(np.asarray([12, 0]))
-    beam.set_xdata([0, beam_x_rot])
-    beam.set_ydata([0, beam_y_rot])
+    # Draw ball, beam, and set point patch
+    ax.plot(0.5,0.049,'r.', ms=50)
+    ax.plot(array([0.0,cos(0.0)]), array([0.0,sin(0.0)]), lw=5)
+    ax.add_patch(Polygon([[0.5, -0.01], [0.5-0.03, -0.05], [0.5+0.03, -0.05]]))
 
-    [ball_x_rot, ball_y_rot] = transformation_matrix.dot(np.asarray([ball_x[i], ball_y]))
-    ball.set_offsets(np.hstack((ball_x_rot, ball_y_rot)))
+    # Draw pivot patch
+    ax.add_patch(Polygon([[0.0,0.0],[0.02,-0.25],[-0.02,-0.25]]))
+    
+    # beam hi and lo limit patches
+    upper_limit = 0.2
+    xa = cos(ulimit)
+    ya = sin(ulimit)
+    ax.add_patch(Polygon([[xa-0.05, ya+0.01],[xa+0.05, ya+0.01], \
+                          [xa+0.05, ya+0.02],[xa-0.05, ya+0.02]]))
+    ax.add_patch(Polygon([[xa-0.05,-ya-0.01],[xa+0.05,-ya-0.01], \
+                          [xa+0.05,-ya-0.02],[xa-0.05,-ya-0.02]]))
+    fig.canvas.draw()
 
-    [target_x_rot, target_y_rot] = transformation_matrix.dot(target_loc)
-    target.set_offsets(np.hstack((target_x_rot, target_y_rot)))
+def update_beam(fig,ax,x,u,SP):
+    # Update the ball and beam diagram with current state values.
+    
+    # Limit beam angle
+    u = -ulimit if u < -ulimit else ulimit if u > ulimit else u
+    
+    # Determine ball position, and if it rolled off the beam
+    x = -0.05 if x < 0 else 1.02 if x > 1.0 else x
+    y = 0.049/cos(u) + x*sin(u) if x >= 0.0 and x <= 1.0 else -0.2
 
-anim = FuncAnimation(
-    fig, animate, interval= 4*env.TIME_INTERVAL*1000, frames = int(len(angles)))
- 
-plt.draw()
+    # update ball, beam, and setpoint positions
+    ax.lines[0].set(xdata=x, ydata=y)
+    ax.lines[1].set(xdata=[0.0,cos(u)], ydata=[0.0,sin(u)])
+    ax.patches[0].set_xy([[SP*cos(u), -0.01 + SP*sin(u)], \
+              [SP*cos(u) - 0.03, -0.05 + SP*sin(u)], \
+              [SP*cos(u) + 0.03, -0.05 + SP*sin(u)]])
+    fig.canvas.draw()
+    
+def draw_plot(fig,ax,SP):
+    
+    # set up xplot to display ball position
+    xplot = ax
+    xplot.set(xlim=(0,tf),ylim=(0,1),xlabel='Time [dimensionless]',ylabel='Position')
+    xplot.plot([],[],'b')
+    xplot.plot([0,tf],[SP,SP],'b--')
+
+    # set up second axist to display beam angle
+    uplot = ax.twinx()
+    uplot.set(xlim=(0,tf),ylim=(-ulimit,ulimit))
+    uplot.set_ylabel('Beam Angle', color='r')
+    for tl in uplot.get_yticklabels():
+        tl.set_color('r')
+    uplot.plot([],[],'r')
+    uplot.plot([0,tf],[0,0],'r--')
+    
+    # return lines for updating
+    return xplot,uplot
+
+def update_plot(ax,t,y):
+    tdata = append(ax.lines[0].get_xdata(),t)
+    ydata = append(ax.lines[0].get_ydata(),y)
+    ax.lines[0].set(xdata=tdata, ydata=ydata)
+
+
+fig,ax = subplots(1,1,figsize=(8,3))
+draw_beam(fig,ax)
+plt.show()
+update_beam(fig,ax,0.5,0.1,0.5)
 plt.show()
