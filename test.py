417eb31bcee5f3be92fd0caebf9d9c59a5edfcbe
Kyle Naddeo <naddeok5@students.rowan.edu>
2020-10-28 14:27:03 -0700

    push to GPU

--- test.py
+++ test.py
@@ -3,16 +3,23 @@ import numpy as np
 from env.BeamEnv import BeamEnv
 from q_learning_agent import QLearner
 
-# Initialize Environment and Agent
-env   = BeamEnv()
-agent = QLearner(env)
-
 # Hyperparameters
 save_q_table = True
 NUMBER_OF_EPISODES = 1e10
 MAX_STEPS = 1000
 EPSILON   = 0.2
 
+# Push to GPU if necessary
+if gpu == True:
+    import os
+    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+    os.environ["CUDA_VISIBLE_DEVICES"] = "3"
+
+# Initialize Environment and Agent
+env   = BeamEnv()
+agent = QLearner(env)
+
+# Train
 num_successes = 0
 for episode in range(NUMBER_OF_EPISODES):
     if episode % 1000 == 0:
