569a94c6b5121b88b56a22aed0520242f55f1cb3
Kyle Naddeo <naddeok5@students.rowan.edu>
2020-10-28 14:19:18 -0700

    training setup

--- test.py
+++ test.py
@@ -1,10 +1,49 @@
 # Imports
-from env import Beam
-from agent import Balancer
-from silence import shh
+import numpy as np
+from env.BeamEnv import BeamEnv
+from q_learning_agent import QLearner
 
+# Initialize Environment and Agent
+env   = BeamEnv()
+agent = QLearner(env)
 
-env = Beam()
-agent = Balancer(env)
-history = agent.episode()
-print(history)
+# Hyperparameters
+save_q_table = True
+NUMBER_OF_EPISODES = 1e10
+MAX_STEPS = 1000
+EPSILON   = 0.2
+
+num_successes = 0
+for episode in range(NUMBER_OF_EPISODES):
+    if episode % 1000 == 0:
+        print("Episode: " + str(episode))
+
+    # Reset Environment
+    state = env.reset()
+    
+    # Experience 
+    done = False
+    count = 0
+    while (not done) and (count > MAX_STEPS):
+
+        # Choose next action (EPSILON-Greedy)
+        action = env.action_space.sample() if np.random.uniform(0, 1) < EPSILON else agent.get_action(state)
+
+        # Next Step
+        next_state, reward, done = env.step(state, action)
+
+        # Update Q Table
+        agent.update_q_table(state, action, reward, next_state)
+
+        # Update Current State
+        state = next_state
+
+        # Update counter
+        count += 1
+        if done:
+            num_successes += 1
+
+print(str(num_successes) + " successes out of " + str(NUMBER_OF_EPISODES) + " episodes." )
+
+if save_q_table:
+    np.savetxt('q_table.csv', agent.q_table, delimiter = "," )
