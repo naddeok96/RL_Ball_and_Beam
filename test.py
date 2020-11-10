e398c529e6ff68af2d8fdcd88a9de338da00a1d5
Kyle Naddeo <naddeok5@students.rowan.edu>
2020-11-10 11:33:57 -0800

    setup test for smaller env

--- test.py
+++ test.py
@@ -4,12 +4,12 @@ from q_learning_agent import QLearner
 import numpy as np
 
 # Hyperparameters
-save_q_table = False
-gpu = False
-NUMBER_OF_EPISODES = 2
-MAX_STEPS = 200
-EPSILON   = 0
-pretrained_q_table = np.loadtxt('pretrained_q_tables/q_table_11_6.csv', delimiter=',')
+save_q_table = True
+gpu = True
+NUMBER_OF_EPISODES = 1e10
+MAX_STEPS = 150
+EPSILON   = 0.1
+# pretrained_q_table = np.loadtxt('pretrained_q_tables/q_table_11_6.csv', delimiter=',')
 
 # Push to GPU if necessary
 if gpu == True:
@@ -18,14 +18,19 @@ if gpu == True:
     os.environ["CUDA_VISIBLE_DEVICES"] = "4"
 
 # Initialize Environment and Agent
-env   = BeamEnv()
+env   = BeamEnv(obs_low_bounds  = np.array([0,   0,  1.18e10, -30]),
+                obs_high_bounds = np.array([6,   6, -1.18e10,  30]), 
+                obs_bin_sizes   = np.array([1, 0.5,        6,   5]))
+
 agent = QLearner(env, 
-                 pretrained_q_table = pretrained_q_table)
+                 learning_rate = 0.20,
+                 discount_factor = 0.80,
+                 pretrained_q_table = None)
 
 # Train
 num_successes = 0
 for episode in range(int(NUMBER_OF_EPISODES)):
-    if episode % (1e3) == 0:
+    if episode % (500) == 0:
         print("Episode: " + str(episode))
 
         if save_q_table:
@@ -60,4 +65,4 @@ for episode in range(int(NUMBER_OF_EPISODES)):
 print(str(num_successes) + " successes out of " + str(NUMBER_OF_EPISODES) + " episodes." )
 
 if save_q_table:
-    np.savetxt('q_table.csv', agent.q_table, delimiter = "," )
+    np.savetxt('q_table_small.csv', agent.q_table, delimiter = "," )
